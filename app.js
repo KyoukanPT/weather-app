72dc35146c99d3da27a77462ffc851199b1f1c5c
KyoukanPT <59366068+KyoukanPT@users.noreply.github.com>
2021-03-31 10:32:44 +0000

    Update app.js

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.post("/", (req, res) => {
             var icon = weatherData.weather[0].icon;
             var icon_url = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-            res.write("<head><meta charset='utf-8'></head>")
-            res.write("<body style='background-color: #87CEFA'>")
+            res.write("<head><meta charset='utf-8'></head>");
+            res.write("<body style='background-color: #87CEFA'>");
             res.write("<div class='container-fluid' style='width: 50%; margin: 15% auto'>");
             res.write("<p><h3>Weather Description: " + weatherDescription + "</h3></p>");
             res.write("<h1>Current Temperature in " + query + " is " + temperature + "&#8451;</h1>");
